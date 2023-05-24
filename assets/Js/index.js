440d8a73048e41bc8d0f64842635577bc9e14519
Afimia Fidelis <38649892+Izebiafe@users.noreply.github.com>
2023-05-24 12:02:35 +0000

    fix linters errors

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,22 +1,22 @@
-const openMenu = document.querySelector("#open-menu");
-const closeMenu = document.querySelector("#close-menu");
-const mobile = document.querySelector("#mobile");
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const mobile = document.querySelector('#mobile');
 
-openMenu.addEventListener("click", () => {
-  mobile.style.display = "block";
-  document.body.style.overflow = "hidden";
+openMenu.addEventListener('click', () => {
+  mobile.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 });
 
-closeMenu.addEventListener("click", () => {
-  mobile.style.display = "none";
-  document.body.style.overflow = "auto";
+closeMenu.addEventListener('click', () => {
+  mobile.style.display = 'none';
+  document.body.style.overflow = 'auto';
 });
 
 // Hide mobile navbar and blurred div
 // for desktop on window resize
 window.onresize = () => {
   if (window.innerWidth >= 768) {
-    mobile.style.display = "none";
-    document.body.style.overflow = "auto";
+    mobile.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 };
